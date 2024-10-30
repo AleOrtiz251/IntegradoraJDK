a869096a1179e6424161cec22eea3d0bce65b732
AleOrtiz251 <24170018@uttcampus.edu.mx>
2024-10-30 10:36:46 -0600

    Hice la hamburguesa negra

--- resources/views/InicioAdmin.blade.php
+++ resources/views/InicioAdmin.blade.php
@@ -74,6 +74,10 @@
             height: 18rem;
             margin: 1rem;
         }
+        .offcanvas{
+            background-color: rgba(0, 0, 0, 0.5);
+        }
+        
     </style>
 </head>
 <body>
@@ -85,24 +89,24 @@
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style="color: white">Offcanvas</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style="color: white"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#"style="color: white">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#"style="color: white">Link</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color: white">Dropdown</a>
+                            <ul class="dropdown-menu" >
+                                <li><a class="dropdown-item" href="#"style="color: white">Action</a></li>
+                                <li><a class="dropdown-item" href="#"style="color: white">Another action</a></li>
                                 <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#"style="color: white">Something else here</a></li>
                             </ul>
                         </li>
                     </ul>
