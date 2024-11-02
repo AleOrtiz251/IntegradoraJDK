cfb1ddd5564071e3415f1648f70c297a3bbbb422
Alexis-Meraz <alexis.meraz2525@gmail.com>
2024-11-02 16:25:42 -0600

    hice cambios en los avisos y agregue la vista grupos y finanzas

--- resources/views/Alumnos/Avisos.blade.php
+++ resources/views/Alumnos/Avisos.blade.php
@@ -20,76 +20,13 @@
             background-color: #072146;
             padding: 1rem 0;
         }
-        .navbar-custom {
+        .container-fluid {
             background-color: #072146;
         }
         .img-logoNav {
             width: 90px;
             height: 90px;
             margin-left: 30px;
-        }
-        .personajesNav {
-            width: 170px;
-            height: 100px;
-        }
-        #botomNav {
-            color: red;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-        .btn-customAvs {
-            background-color: red;
-            color: white;
-            border: 2px solid #072146;
-            padding: 1rem 1rem;
-            border-radius: 10px;
-            text-decoration: none;
-            cursor: pointer;
-        }
-        #contenedorBotom {
-            color: white;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-        .btn-customCon {
-            background-color: #072146;
-            color: white;
-            border: 2px solid #072146;
-            padding: 2rem 2rem;
-            border-radius: 10px;
-            text-decoration: none;
-            cursor: pointer;
-        }
-        .btn-customCon:active {
-            color:  #0E2A52;
-            box-shadow: 0 0.2rem #dfd9d9;
-            transform: translateY(0.2rem);
-        }
-        .btn-customCon:hover:not(:disabled) {
-            background:  #0E2A52;
-            color: white;
-            text-shadow: 0 0.1rem #bcb4b4;
-        }
-        .btn-customAcc {
-            background-color: #0E2A52;
-            color: white;
-            border: 2px solid #072146;
-            padding: 2rem 2rem;
-            border-radius: 10px;
-            text-decoration: none;
-            cursor: pointer;
-        }
-        .btn-customAcc:active {
-            color: white;
-            box-shadow: 0 0.2rem #dfd9d9;
-            transform: translateY(0.2rem);
-        }
-        .btn-customAcc:hover:not(:disabled) {
-            background: white;
-            color: #072146;
-            text-shadow: 0 0.1rem #bcb4b4;
         }
         .header-text {
             color: white;
@@ -107,77 +44,99 @@
             height: 28px;
             margin-right: 5px;
         }
-        .carousel-item {
-            position: relative;
+        .navbar-toggler {
+            background-color: white;
         }
-        .carousel-caption {
-            position: absolute;
-            bottom: 20%;
-            left: 50%;
-            transform: translateX(-50%);
-            background-color: rgba(0, 0, 0, 0.5);
-            padding: 1rem;
+        .background-div {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+        .table {
+           max-width: 80%;
+           min-height: 80%;
+           justify-content: end;
+        }
+        .inner-container {
+            background: rgba(255, 255, 255, 0.8);
+            padding: 0.5rem;
             border-radius: 10px;
-        }
-        
-        #container-fluid{
-            background-color: #072146;
-            min-height: 100vh;
+            width: 50vw;
+            height: 50vh;
             display: flex;
             flex-direction: column;
-             margin: 0;
+            justify-content: center;
+            align-items: center;
+        }
+        .card {
+            width: 18rem;
+            height: 18rem;
+            margin: 1rem;
+        }
+        .offcanvas{
+            background-color: rgba(0, 0, 0, 0.5);
         }
         
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+    <nav class="navbar">
         <div class="container-fluid">
-            <div class="row w-100">
-                <div class="col-4 d-flex align-items-center">
-                    <img src="{{ asset('images/JDKJulietasLogoBlanco.png') }}" alt="" class="img-logoNav">
+            <a class="navbar-brand" href="#"><img src="{{ asset('images/JDKJulietasLogoBlanco.png') }}" alt="" class="img-logoNav"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style="color: white">Offcanvas</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style="color: white"></button>
                 </div>
-                <div class="col-4 d-flex justify-content-center">
-                    <img src="{{ asset('images/MonosBlanco.png') }}" alt="" class="personajesNav">
-                </div>
-                <div class="col-4 d-flex justify-content-end align-items-center" id="contenedorBotom">
-                    <a href="#" class="btn-customCon">Conócenos</a>
-                    <a href="#" class="btn-customAcc">Cuenta</a>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"style="color: white">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"style="color: white">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color: white">Dropdown</a>
+                            <ul class="dropdown-menu" >
+                                <li><a class="dropdown-item" href="#"style="color: white">Action</a></li>
+                                <li><a class="dropdown-item" href="#"style="color: white">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#"style="color: white">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </div>
     </nav>
-
-    <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
-        <div class="container-fluid">
-            <div class="row w-100">
-                <div class="col d-flex justify-content-center align-items-center" id="botomNav">
-                    <a href="#" class="btn-customAvs">avisos</a>
-                    <a href="#" class="btn-customAvs">grupos</a>
-                    <a href="#" class="btn-customAvs">finanzas</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container bg-secondary">
-        <div class="row">
-            <table class="table table-bordered" >
+    <div class="background-div">
+        <div class="table">
+            <table class="table table-bordered">
                 <tr class="table-dark">
-                    <th class = "col-2">Nombre</th>
-                    <th class="col-8">Mensaje</th>
-                    <th class = "col-4">Fecha</th>
+                    <th class = "col-2">Profesor</th>
+                    <th class = "col-8">Mensaje</th>
+                    <th class = "col-2">Fecha</th>
                 </tr>
             </table>
         </div>
     </div>
-
     <footer class="text-center">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-4 d-flex flex-column align-items-center justify-content-center footer-link">
-                    <p>📞 8717976623</p>
-                    <p>📍<a href="https://maps.app.goo.gl/DCgQmxAuw8RjsNTo6" class="footer-link">Querétaro 120, La Merced, 27296 Torreón, Coah.</a></p>
+                    <p>8717976623</p>
+                    <p><a href="https://maps.app.goo.gl/DCgQmxAuw8RjsNTo6" class="footer-link">Querétaro 120, La Merced, 27296 Torreón, Coah.</a></p>
                 </div>
                 <div class="col-4 d-flex align-items-center justify-content-center">
                     <img src="{{ asset('images/JDKJulietasLogoBlanco.png') }}" alt="" class="footer-logo">
