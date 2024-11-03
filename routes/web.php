7363e91851b020c306ab9259b2503536278ce2fb
YamilRA <jyra010920@gmail.com>
2024-11-02 20:49:12 -0600

    a

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JidokwanController;
-use App\Models\Custom_user;
 
-Route::get('/',[JidokwanController::class,'vistaprincipal']);
 
-Route::get('/login',[JidokwanController::class,'getlogin']);
-
-Route::get('/cexamen',[JidokwanController::class,'crearexamen']);
-
-Route::get('/cclase',[JidokwanController::class,'crearclase']);
-
-/*Route::get('/admin', function () {
-    return view('Admin/InicioAdmin');
-});
-
-Route :: get('/alumno/avisos', function () {
-    return view('Alumno.avisos');
-});
-
-Route :: get('/formulrio', function () {
-    return view('form');
-});*/
 
