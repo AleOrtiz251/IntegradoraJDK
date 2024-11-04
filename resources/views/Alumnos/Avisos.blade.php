bdc9c1d653a994db5696f0f96bf6e0c01b8f0542
Alexis-Meraz <alexis.meraz2525@gmail.com>
2024-11-04 14:09:39 -0600

    modifique el fondo de los avisos

--- resources/views/Alumnos/Avisos.blade.php
+++ resources/views/Alumnos/Avisos.blade.php
@@ -45,9 +45,10 @@
             margin-right: 5px;
         }
         .navbar-toggler {
-            background-color: white;
+            background-color: rgba(127, 144, 245, 0.473);
         }
         .background-div {
+            background-color: rgba(127, 144, 245, 0.473);
             width: 100%;
             height: 100%;
             display: flex;
@@ -91,31 +92,27 @@
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style="color: white">Offcanvas</h5>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style="color: white">Menu</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style="color: white"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#"style="color: white">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#"style="color: white">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"style="color: white">Link</a>
+                            <a class="nav-link" href="#"style="color: white">Progreso</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="color: white">Dropdown</a>
-                            <ul class="dropdown-menu" >
-                                <li><a class="dropdown-item" href="#"style="color: white">Action</a></li>
-                                <li><a class="dropdown-item" href="#"style="color: white">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#"style="color: white">Something else here</a></li>
-                            </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"style="color: white">Avisos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"style="color: white">Grupos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"style="color: white">Finanzas</a>
                         </li>
                     </ul>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </div>
