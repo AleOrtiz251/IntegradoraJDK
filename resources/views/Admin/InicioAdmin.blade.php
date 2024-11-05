ab0230144b912da15771b9c32f00e7a1c6d556a6
AleOrtiz251 <24170018@uttcampus.edu.mx>
2024-11-05 14:51:47 -0600

    acambio de notificaciones

--- resources/views/Admin/InicioAdmin.blade.php
+++ resources/views/Admin/InicioAdmin.blade.php
@@ -101,6 +101,12 @@
         .offcanvas {
             background-color: rgba(0, 0, 0, 0.5);
         }
+
+        .table {
+           max-width: 80%;
+           min-height: 80%;
+           justify-content: end;
+        }
     </style>
 </head>
 <body>
@@ -144,44 +150,14 @@
         </div>
     </nav>
     <div class="background-div">
-        <div class="inner-container">
-            <h2>Notificaciones</h2>
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="card w-100">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="card w-100">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="card w-100">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Anterior</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Siguiente</span>
-                </button>
-            </div>
+    <div class="table">
+            <table class="table table-bordered">
+                <tr class="table-dark">
+                    <th class = "col-2">Profesor</th>
+                    <th class = "col-8">Mensaje</th>
+                    <th class = "col-2">Fecha</th>
+                </tr>
+            </table>
         </div>
     </div>
     <footer class="text-center">
